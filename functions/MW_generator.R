93f60ef102a0ccbfcc1b778a1d1d4a377b046e76
Your Name <taioh.yokoyama@etu.unistra.fr>
2021-07-12 16:51:55 +0200

    TY : 12.07.21 minor corrections

--- functions/MW_generator.R
+++ functions/MW_generator.R
@@ -14,7 +14,7 @@
 MW_generator <- function(formula, data, id=NULL){
   
   # Requirements
-  lapply(c("lme4","varhandle", "parallel"),
+  lapply(c("lme4","varhandle","purrr", "parallel"),
          require, 
          character.only = T)
   
@@ -25,6 +25,7 @@ MW_generator <- function(formula, data, id=NULL){
   
   # Dummy variables
   df.dummy <- as.data.frame(to.dummy(data[,VarTrt], "tt"))
+  dummy.names <- names(df.dummy)
   df <- cbind(data, df.dummy)
   
   # No random effects
@@ -44,13 +45,25 @@ MW_generator <- function(formula, data, id=NULL){
   }
   
   # Output dataframe
-  l_preds <- predict(l_mods, newdata = df, type = "response")
+  
+  NIPls <- lapply(c(1:length(l_mods)),FUN = function(i){
+    unique(as.numeric(row.names(ranef(l_mods[[i]])$NIP)))
+  })
+  
+  length_NIPls <- lapply(c(1:length(NIPls)), FUN = function(i){
+    length(NIPls[[i]])
+  })
+  
+  indice <- min(which(min(unique(unlist(length_NIPls)))==unlist(length_NIPls)))
+  newdata <-  df %>%
+      filter(NIP %in% NIPls[[indice]])
+  l_preds <- predict(l_mods, newdata, type = "response")
   t_pred <- as.data.frame(Reduce(cbind,l_preds))
   names(t_pred) <- paste("e", c(1:length(t_pred)), sep = "")
   t_pred$e.min <- do.call(pmin, t_pred)
-  denominator <- rowSums(t_pred[,c(1:4)]*df.dummy)
-  data$MW <- t_pred$e.min/denominator
+  denominator <- rowSums(t_pred[,c(1:4)]*newdata[,dummy.names])
+  newdata$MW <- t_pred$e.min/denominator
   
-  return(data)
+  return(newdata)
 }
 
